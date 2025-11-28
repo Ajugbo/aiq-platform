0bf46368d2adc8cf65ada2b80c68422bedce3d2a
Ajugbo <davidmanga19@gmail.com>
2025-11-28 20:32:56 +0100

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsNELEz9c0V0eP5swic8TjQFn24xfl32c",
+  authDomain: "aiq-platform-7eba9.firebaseapp.com",
+  databaseURL: "https://aiq-platform-7eba9-default-rtdb.firebaseio.com",
+  projectId: "aiq-platform-7eba9",
+  storageBucket: "aiq-platform-7eba9.firebasestorage.app",
+  messagingSenderId: "500857348774",
+  appId: "1:500857348774:web:a7c188037b77f4d5daca67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
